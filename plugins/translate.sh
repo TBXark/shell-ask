e946c21c170049ee8ac215e557eb4efdd5765214
tbxark <tbxark@outlook.com>
2024-05-23 11:23:06 +0800

    doc: 添加翻译插件示例注释

--- plugins/translate.sh
+++ plugins/translate.sh
@@ -5,15 +5,16 @@
 
 # Plugin scripts must include a gen_content function, which takes two parameters. The first parameter is the user's input and the second parameter is the pipeline input.
 gen_content() {
-    question=$1 # target language
-    context=$2 # need translation text
+    question=$1 # user's input： target language
+    context=$2  # pipeline input：need translation text
 
     # You can only have one /dev/stdin output. If your other commands may also cause output, you need to redirect them elsewhere.
     echo "Translate the following text into $question: $context"
 }
 
 
+# You can add an "after_ask" function here to customize the processing of AI's return results. You can also choose not to implement it, in which case the default behavior is to output the result.
 after_ask() {
-    local result=$1
+    local result=$1 # AI's return results
     echo $result
 }
