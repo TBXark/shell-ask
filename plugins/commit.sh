1784b51feed3a3cd92b9be1223b9662d7bfec657
tbxark <tbxark@outlook.com>
2024-11-22 16:25:03 +0800

    refactor: 更新生成提交消息的提示信息，添加提交消息格式说明

--- plugins/commit.sh
+++ plugins/commit.sh
@@ -1,10 +1,6 @@
 # Example: Execute the command in the current directory to obtain additional input.
 
 gen_content() {
-    if git diff --cached --exit-code; then
-        echo "No changes to commit"
-        exit 1
-    fi
     local context=$(git diff --cached)
-    echo "Generate git commit messages based on git diff output according to the standard commit specification. You must return only the commit message without any other text or quotes. Here is the git diff output:\n$context"
+    echo "Generate git commit messages based on git diff output according to the standard commit specification. You must return only the commit message without any other text or quotes. Format of the Commit Message: {type}: {subject}. Allowed Types: feat, fix, docs, style, refactor, test, chore\n. Here is the git diff output:\n$context"
 }
